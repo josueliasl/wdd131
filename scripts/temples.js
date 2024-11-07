4def81fd38c247e2c45cd22364fbdb9a82f00746
josueliasl <josue.elias.leyva@gmail.com>
2024-11-07 19:45:18 +0100

    final-frist-test

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,3 +1,11 @@
+const mainnav = document.querySelector('.navigation')
+const hambutton = document.querySelector('#menu');
+
+hambutton.addEventListener('click', () =>{
+    mainnav.classList.toggle('show');
+    hambutton.classList.toggle('show')
+})
+
 let currentYear = new Date().getFullYear();
 let lastModified = document.lastModified;
 
