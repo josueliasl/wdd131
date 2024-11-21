71d1ef78ec5704c28339084a92251e0d40ee5274
josueliasl <josue.elias.leyva@gmail.com>
2024-11-21 20:48:32 +0100

    done

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -2,4 +2,4 @@ let currentYear = new Date().getFullYear();
 let lastModified = document.lastModified;
 
 document.getElementById("currentYear").textContent = currentYear;
-document.getElementById("lastModified").textContent = `Last Modification: ${lastModified}`;
+document.getElementById("lastModified").textContent = ` Last Modification: ${lastModified}`;
