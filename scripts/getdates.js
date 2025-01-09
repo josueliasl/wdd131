291e4d98f49666fa8be7ea8ba7d489305c8450c0
josueliasl <josue.elias.leyva@gmail.com>
2025-01-08 22:44:48 -0600

    Linking styles, scripts and getting the year

---
+++ scripts/getdates.js
@@ -0,0 +1,5 @@
+let currentYear = new Date().getFullYear();
+let lastModified = document.lastModified;
+
+document.getElementById("currentYear").textContent = currentYear;
+document.getElementById("lastModified").textContent = ` Last Modification: ${lastModified}`;
